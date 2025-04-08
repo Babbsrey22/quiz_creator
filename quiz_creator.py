fa27575111d832010e9c9f30294b5d39a4370b16
Eunice <reyes05eunice@gmail.com>
2025-04-08 21:43:59 +0800

    First commit + Pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,5 @@
+# Ask user to input questions
+# Ask user to input 4 choices of answers (a,b,c,d)
+# Input the correct answer
+# Repeat until user inputs 'No'
+# Store input into text file (file.write)
