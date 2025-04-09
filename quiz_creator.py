3be7584e1ee2b352a85d92cf1ba6ec46bac5c0cf
Eunice <reyes05eunice@gmail.com>
2025-04-09 12:32:53 +0800

    Asks user to input question and choices

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,12 @@
-# Ask user to input questions
-# Ask user to input 4 choices of answers (a,b,c,d)
+def question_creator():
+    idx = 0
+    while True:
+        question = input(f"\nPress 0 or any key to exit\nEnter question number {idx + 1}: ")
+        choices = [choice_a, choice_b, choice_c, choice_d]
+        choice_a = input("Option a: ")
+        choice_b = input("Option b: ")
+        choice_c = input("Option c: ")
+        choice_d = input("Option d: ")
 # Input the correct answer
 # Repeat until user inputs 'No'
 # Store input into text file (file.write)
