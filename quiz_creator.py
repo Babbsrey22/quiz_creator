0f0beed03a4e404f380feb871311bb16efd6e205
Eunice <reyes05eunice@gmail.com>
2025-04-09 13:04:28 +0800

    Fixed question and answer input. added option for correct answer

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,12 +1,26 @@
 def question_creator():
-    idx = 0
     while True:
-        question = input(f"\nPress 0 or any key to exit\nEnter question number {idx + 1}: ")
-        choices = [choice_a, choice_b, choice_c, choice_d]
+        question = input(f"\nPress 0 to exit\nEnter question: ")
+        if question == "0":
+            print("Stopping Quiz Creator....")
+            break
+        else: 
+            choices()
+
+def choices():
         choice_a = input("Option a: ")
         choice_b = input("Option b: ")
         choice_c = input("Option c: ")
         choice_d = input("Option d: ")
-# Input the correct answer
-# Repeat until user inputs 'No'
+
+        while True:
+            correct_answer = input("Enter the correct option (a/b/c/d): ")
+            if correct_answer in ["a", "b", "c", "d"]:
+                break
+            else:
+                print("Please enter a valid option")
+                
+
+question_creator()
+
 # Store input into text file (file.write)
