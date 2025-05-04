84114b7c79da606899067e17a369b97a2aa9585a
Eunice <reyes05eunice@gmail.com>
2025-05-04 16:07:11 +0800

    Pseudocode (reupload due to not saving beforehand)

---
+++ quiz_sim_with_gui.py
@@ -0,0 +1,5 @@
+from tkinter import *
+
+# Create simple GUI
+# Transfer code from quiz_simpulator.py here and adjust/add things if needed
+# Add timer feature and view previous quiz scores if possible
