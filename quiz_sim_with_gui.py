267167a311b5de4891267c6ef33ff1f4a1f1d8da
Eunice Reyes <reyes05eunice@gmail.com>
2025-05-05 12:38:15 +0800

    Delete quiz_sim_with_gui.py

--- quiz_sim_with_gui.py
+++
@@ -1,5 +0,0 @@
-from tkinter import *
-
-# Create simple GUI
-# Transfer code from quiz_simpulator.py here and adjust/add things if needed
-# Add timer feature and view previous quiz scores if possible
